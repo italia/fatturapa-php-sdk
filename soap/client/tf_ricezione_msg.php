9de74fef8e3849832dcb86d0e95de8e546e5c858
e.gennari <e.gennari@ellecisystem.it>
2018-06-28 10:23:15 +0200

    trasmissione sdi ricezione messaggi

--- soap/client/tf_ricezione_msg.php
+++ soap/client/tf_ricezione_msg.php
@@ -1,8 +1,45 @@
 <?php
-
 include("../config.php");
 
 $client = new SoapClient( $eptft, array( 'cache_wsdl' => WSDL_CACHE_NONE ));
 
+function print_output($response)
+{
+	if ($response == "") return "OK";
+
+	return($response);
+}
+
+echo "<h1>Test ricezione msg da SDI in seguito ad invio fattura</h1>";
+
+echo "<hr />";
+echo "<h2>Ricevuta consegna</h2>";
 $response = $client->ricevutaConsegna();
-echo $response;
+print_r(print_output($response));
+
+echo "<hr />";
+echo "<h2>Notifica mancata consegna</h2>";
+$response = $client->notificaMancataConsegna();
+print_r(print_output($response));
+
+echo "<hr />";
+echo "<h2>Notifica scarto</h2>";
+$response = $client->notificaScarto();
+print_r(print_output($response));
+
+echo "<hr />";
+echo "<h2>Notifica esito</h2>";
+$response = $client->notificaEsito();
+print_r(print_output($response));
+
+echo "<hr />";
+echo "<h2>Notifica decorrenza termini</h2>";
+$response = $client->notificaDecorrenzaTermini();
+print_r(print_output($response));
+
+echo "<hr />";
+echo "<h2>Attestazione trasmissione fattura</h2>";
+$response = $client->attestazioneTrasmissioneFattura();
+print_r(print_output($response));
+
+
--- soap/server/trasmissione-fatture.php
+++ soap/server/trasmissione-fatture.php
@@ -1,8 +1,14 @@
 <?php
 ini_set( "soap.wsdl_cache_enabled", 0 );
 ini_set( 'soap.wsdl_cache_ttl', 0 );
-
 include("../config.php");
+
+/*
+*	I seguenti end point vengono esposti dal trasmittente
+*	per ricevere i messaggi inviati dall'SDI
+*	in seguito alla trasmissione di una fattura.
+*	E' possibile simulare le chiamate a questi end point dal file /client/tf_ricezione_msg.php
+*/
 
 function ricevutaConsegna() {
   
@@ -16,10 +22,38 @@ function notificaMancataConsegna() {
 
 }
 
+function notificaScarto() {
+
+	//parse request
+
+}
+
+function notificaEsito() {
+
+	//parse request
+
+}
+
+function notificaDecorrenzaTermini() {
+
+	//parse request
+
+}
+
+function attestazioneTrasmissioneFattura() {
+
+	//parse request
+
+}
+
 $server = new SoapServer( $tfwsdl );
 
 $server->addFunction( "ricevutaConsegna" );
 $server->addFunction( "notificaMancataConsegna" );
+$server->addFunction( "notificaScarto" );
+$server->addFunction( "notificaEsito" );
+$server->addFunction( "notificaDecorrenzaTermini" );
+$server->addFunction( "attestazioneTrasmissioneFattura" );
 
 $server->handle();
 
