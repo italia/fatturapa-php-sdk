e556ee072b57345b01e863efac9e6b164c27f81d
Riccardo Mariani <riccardo.mariani@gmail.com>
2018-06-30 00:06:33 +0200

    simplexml_load_file

---
+++ utils/viusual-pa/upload.php
@@ -0,0 +1,41 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($FileType != "xml") {
+    echo "Sorry, only xml files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        read();
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
+function read() {
+    $path = $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']) . "/" . basename($_FILES["fileToUpload"]["name"]);    
+    $xml = simplexml_load_file($path);
+    var_dump($xml);
+}
+?>
