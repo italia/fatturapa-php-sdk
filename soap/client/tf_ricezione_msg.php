b29896f4695ae088fd14565aa3d3bb4a1b062a94
e.gennari <e.gennari@ellecisystem.it>
2018-06-30 08:03:30 +0200

    fix wsdl for test on vm

--- soap/client/tf_ricezione_msg.php
+++ soap/client/tf_ricezione_msg.php
@@ -3,17 +3,7 @@ ini_set( "soap.wsdl_cache_enabled", 0 );
 ini_set( 'soap.wsdl_cache_ttl', 0 );
 include("../config.php");
 
-$client = new SoapClient($eptft, array(
-    //'trace' => 1,
-    'stream_context' => stream_context_create(array(
-    	'cache_wsdl' => WSDL_CACHE_NONE,
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false, 
-            'allow_self_signed' => true //can fiddle with this one.
-        )
-    ))
- ));
+$client = new SoapClient( $eptft, array( 'cache_wsdl' => WSDL_CACHE_NONE ));
 
 function print_output($response)
 {
@@ -24,34 +14,34 @@ function print_output($response)
 
 echo "<h1>Test ricezione msg da SDI in seguito ad invio fattura</h1>";
 
-echo "<hr />";
-echo "<h2>Ricevuta consegna</h2>";
-$response = $client->ricevutaConsegna();
-print_r(print_output($response));
+	echo "<hr />";
+	echo "<h2>Ricevuta consegna</h2>";
+	$response = $client->ricevutaConsegna();
+	print_r(print_output($response));
 
-echo "<hr />";
-echo "<h2>Notifica mancata consegna</h2>";
-$response = $client->notificaMancataConsegna();
-print_r(print_output($response));
+	echo "<hr />";
+	echo "<h2>Notifica mancata consegna</h2>";
+	$response = $client->notificaMancataConsegna();
+	print_r(print_output($response));
 
-echo "<hr />";
-echo "<h2>Notifica scarto</h2>";
-$response = $client->notificaScarto();
-print_r(print_output($response));
+	echo "<hr />";
+	echo "<h2>Notifica scarto</h2>";
+	$response = $client->notificaScarto();
+	print_r(print_output($response));
 
-echo "<hr />";
-echo "<h2>Notifica esito</h2>";
-$response = $client->notificaEsito();
-print_r(print_output($response));
+	echo "<hr />";
+	echo "<h2>Notifica esito</h2>";
+	$response = $client->notificaEsito();
+	print_r(print_output($response));
 
-echo "<hr />";
-echo "<h2>Notifica decorrenza termini</h2>";
-$response = $client->notificaDecorrenzaTermini();
-print_r(print_output($response));
+	echo "<hr />";
+	echo "<h2>Notifica decorrenza termini</h2>";
+	$response = $client->notificaDecorrenzaTermini();
+	print_r(print_output($response));
 
-echo "<hr />";
-echo "<h2>Attestazione trasmissione fattura</h2>";
-$response = $client->attestazioneTrasmissioneFattura();
-print_r(print_output($response));
+	echo "<hr />";
+	echo "<h2>Attestazione trasmissione fattura</h2>";
+	$response = $client->attestazioneTrasmissioneFattura();
+	print_r(print_output($response));
 
 
