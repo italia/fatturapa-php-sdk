b0a86d494cf15070e59a304003de03e0b9c510b7
Riccardo Mariani <riccardo.mariani@gmail.com>
2018-07-02 18:07:27 +0200

    xmlfattura con css

--- utils/viusual-pa/upload.php
+++ utils/viusual-pa/upload.php
@@ -4,23 +4,24 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-/* Check if file already exists
+// Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
-*/
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
+
 // Allow certain file formats
 if($FileType != "xml") {
     echo "Sorry, only xml files are allowed.";
     $uploadOk = 0;
 }
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
