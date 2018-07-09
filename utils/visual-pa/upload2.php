ad35d231656cdc5623bc140942e9446baa924ff9
Riccardo Mariani <riccardo.mariani@gmail.com>
2018-07-09 17:01:29 +0200

    xml json

---
+++ utils/visual-pa/upload2.php
@@ -0,0 +1,46 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+/* Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+*/
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($FileType != "xml") {
+    echo "Sorry, only xml files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        read(basename($_FILES["fileToUpload"]["name"]), $target_dir);
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
+function read($filename, $target_dir) {
+    $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
+    $path = $protocol . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . "/" . $target_dir . $filename;
+    $xml = simplexml_load_file($path);
+
+    $json = json_encode($xml);
+    $array = json_decode($json,TRUE);
+    print_r($array);
+}
